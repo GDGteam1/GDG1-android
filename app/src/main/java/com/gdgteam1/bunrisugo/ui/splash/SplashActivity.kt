3b9ed4b2d5ee400f6c7d76826a9ccec60ae67e82
Jong-Han <jonghan9611@gmail.com>
2022-06-25 17:43:46 +0900

    SplashActivity 추가

---
+++ app/src/main/java/com/gdgteam1/bunrisugo/ui/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.gdgteam1.bunrisugo.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gdgteam1.bunrisugo.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
